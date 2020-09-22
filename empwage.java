27fd171d696715edb300d4e46e4f8b38ba2e765d
diksha-kalra <kalradiksha11@gmail.com>
2020-09-22 23:46:53 +0530

    [diksha] : after merging all branches in master file

--- empwage.java
+++ empwage.java
@@ -8,15 +8,29 @@ public class empwage {
 	public static final int WORKING_DAY_PER_MONTH=20;
 	public static final int TOTAL_WORKING_HRS=100;
 	public static final int TOTAL_WORKING_DAYS=20;
+	//public static final int IS_PRESENT =1;  // use for checking emp present or not 
 		
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
+		// To check whether employee is present or not
+ 		
+//		double emp_check = Math.floor(Math.random()*10)%2;
+//		
+//		//Checking whether employee is present or not 
+//		if(emp_check == IS_PRESENT) {
+//			System.out.println("Employee Present");
+//		}
+//		else {
+//			System.out.println("Employee Absent");
+//		}
+//		
+			
 		//Variable
 		int emp_hours=0; 
-//		int daily_emp_wage=0; use this variable when computing daily wage
-//		int monthly_wage=0; use this variable when computing monthly wage
+//		int daily_emp_wage=0; //use this variable when computing daily wage
+//		int monthly_wage=0; //use this variable when computing monthly wage
 		int emp_wage=0;
 		
 		double emp_check = Math.floor(Math.random()*10)%3;
@@ -25,7 +39,7 @@ public class empwage {
 		if(emp_check == IS_FULL_TIME) {
 			System.out.println("Employee is full time");
 			emp_hours=8;
-			emp_wage=TOTAL_WORKING_HRS*WAGE_PER_HOUR; // when using 100hrs for wage calc. where 100hrs is max hrs full time emp can work for in a month
+			emp_wage=TOTAL_WORKING_HRS*WAGE_PER_HOUR; // when using 100hrs for wage calc. where 100hrs is max hrs full time emp. can work for in a month
 			System.out.println("Full Time Employee Wage "+ emp_wage);// when using above condn then use this print statement
 		}
 		else if (emp_check== IS_PART_TIME) {
@@ -40,7 +54,7 @@ public class empwage {
 			System.out.println("Employee absent");
 			emp_hours=0;
 		}
-// usin switch statements
+// using switch statements
 //		//Variable
 //		int emp_hours=0;
 //		int daily_emp_wage=0;
