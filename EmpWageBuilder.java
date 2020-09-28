220fc4fcfc9b727f96e31f33c7c566f2049b7df0
diksha-kalra <kalradiksha11@gmail.com>
2020-09-29 02:14:00 +0530

    Added: Using Array List to Compute Wages

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,38 +1,42 @@
 package emp;
+import java.util.*;
 
 public class EmpWageBuilder {
 	//constant
 	public static final int IS_FULL_TIME =1;
 	public static final int IS_PART_TIME=2;
+	Scanner sc=new Scanner(System.in);
 	
-	//Variables
-	private int num_of_company=0;
-	private CompanyEmpWage [] comapny_emp_wage_array;
+	//ArrayList
+	public ArrayList<CompanyEmpWage> company_emp_wage=new ArrayList<CompanyEmpWage>();
 	
-	//Constructor
-	public EmpWageBuilder() {
-		comapny_emp_wage_array=new CompanyEmpWage[4];
+	//setter
+	public void setCompany(ArrayList<CompanyEmpWage> company_emp_wage) {
+		this.company_emp_wage=company_emp_wage;
 	}
 	
-	private void addCompanyEmpWage(int emp_wage_per_hr, int working_day_per_month, int working_hrs_per_month, String company_name) {
-		comapny_emp_wage_array[num_of_company]=new CompanyEmpWage(emp_wage_per_hr,working_day_per_month,working_hrs_per_month,company_name);
-		num_of_company++;
+	//getter
+	public ArrayList<CompanyEmpWage> getCompany() {
+		return company_emp_wage;
 	}
 	
-	private void computeEmpWage() {
-		for(int i=0;i<num_of_company;i++) {
-			int total_emp_wage=this.computeEmpWage(comapny_emp_wage_array[i]);
-			System.out.println("Total Emp Wage for Company "+comapny_emp_wage_array[i].company_name +" is: "+total_emp_wage);
-		}
+	//Method to add contact person details
+	public void addCompany(CompanyEmpWage pobj ) {
+		company_emp_wage.add(pobj);
 	}
+	
+	//method to view company employee details
+	public ArrayList<CompanyEmpWage> viewAllDetails(){
+		return company_emp_wage;
+	}	
 	
 	//Method to compute Company Wage
-	private int computeEmpWage(CompanyEmpWage companyEmpWage) {
+	public int computeEmpWage(int emp_wage_per_hr, int working_day_per_month, int working_hrs_per_month, String company_name) {
 		int emp_hrs_per_day;
 		int total_hrs=0;
 		int total_working_days=0;
 		System.out.println("Day\t Hours Worked\tTotal Wage");
-		while(total_hrs<=companyEmpWage.working_hrs_per_month && total_working_days<companyEmpWage.working_day_per_month) {
+		while(total_hrs<=working_hrs_per_month && total_working_days<working_day_per_month) {
 		total_working_days++;
 		int emp_check= (int) Math.floor(Math.random()*10)%3;
 		switch(emp_check) {
@@ -46,22 +50,14 @@ public class EmpWageBuilder {
 			emp_hrs_per_day=0;
 			
 		}
-		int emp_wage=emp_hrs_per_day*companyEmpWage.emp_wage_per_hr;
+		int emp_wage=emp_hrs_per_day*emp_wage_per_hr;
 		total_hrs+=emp_hrs_per_day;
+		total_working_days++;
 		System.out.println(" "+total_working_days+" \t"+total_hrs+" \t\t"+emp_wage);
 	}
-		return total_hrs*companyEmpWage.emp_wage_per_hr;
+		return total_hrs*emp_wage_per_hr;
 	}
-	
-	public static void main(String[] args) {
-		EmpWageBuilder emp_wage_builder=new EmpWageBuilder();
-		emp_wage_builder.addCompanyEmpWage(40,20,70,"capgemini");
-		emp_wage_builder.addCompanyEmpWage(50,25,100,"IBM");
-		emp_wage_builder.computeEmpWage();
-		
-		
 	}
-	
-}
+
 	
 		
---
+++ WageCalculation.java
@@ -0,0 +1,29 @@
+package emp;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+public class WageCalculation {
+	public static void main(String[] args) {
+		
+		//object of builder class
+		EmpWageBuilder emp=new EmpWageBuilder();
+		//scanner class to take user inputs
+		Scanner obj = new Scanner(System.in);
+		//object of company emp wage class
+		CompanyEmpWage c=new CompanyEmpWage();
+		//setting the values of company emp wage class
+		System.out.println("Enter Company name");
+		c.setCompany_name(obj.next());
+		System.out.println("Enter Total Working Hours");
+		c.setWorking_hrs_per_month(obj.nextInt());
+		System.out.println("Enter Total Working Days");
+		c.setWorking_day_per_month(obj.nextInt());
+		System.out.println("Enter Wage per hour");
+		c.setEmp_wage_per_hr(obj.nextInt());
+		emp.addCompany(c);
+		int emp_wage=emp.computeEmpWage(c.getEmp_wage_per_hr(),c.getWorking_day_per_month(),c.getWorking_hrs_per_month(),c.getCompany_name());
+		System.out.println("Company Wage :"+emp_wage);
+
+	}
+}
